94668e733f4f22cffa5909494198ae50f63904ca
florentd <fdamiens@itnetwork.fr>
2023-09-01 10:42:03 +0200

    Fix css baseline

--- front/pages/admin/index.tsx
+++ front/pages/admin/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
+import {CssBaseline} from "@mui/material";
 
 const App = dynamic(() => import("../../components/admin/Admin"), { ssr: false });
 
 const Admin: NextPage = () => {
-    return <App />;
+  return (
+    <>
+      <CssBaseline />
+      <App />
+    </>
+  );
 };
 
 export default Admin;
